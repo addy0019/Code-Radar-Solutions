e2b343792412934513bf0ad6c1c0908d6f7654a0
addy0019 <aditya0019.becse24@chitkara.edu.in>
2025-02-09 15:17:34 +0530

    Solution to Check Equal To

--- Check Equal To@@67882a731b4392d8fc1577da/code.c
+++ Check Equal To@@67882a731b4392d8fc1577da/code.c
@@ -3,9 +3,9 @@ int main() {
     int num1, num2;
     scanf("%d %d", &num1, &num2);
     if(num1 == num2){
-        printf(True\n);
+        printf("True\n");
     }else{
-        printf(False\n);
+        printf("False\n");
     }
     return 0;
 
